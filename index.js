83a1dc0626206e3ce3a8326dc649d6d7e437f290
Sharon Gacheru <sharongacheru2@gmail.com>
2025-04-23 13:56:41 +0300

    updated

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const employee = {
-    name: "John Doe",
-    streetAddress: "123 Main St",
+  name: "Sharon",
+  streetAddress: "123 Nairobi Ave"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
   };
-  
-  function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return { ...employee, [key]: value };
-  }
-  
-  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
-  }
-  
-  function deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = { ...employee };
-    delete newEmployee[key];
-    return newEmployee;
-  }
-  
-  function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
-    return employee;
-  }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
